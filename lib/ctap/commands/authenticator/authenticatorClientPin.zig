009d077c43ab34bab7a7b25eb08f32c356756c21
David Sugar <david@thesugar.de>
2026-02-16 00:55:35 +0100

    implemented pin change

--- lib/ctap/commands/authenticator/authenticatorClientPin.zig
+++ lib/ctap/commands/authenticator/authenticatorClientPin.zig
@@ -57,6 +57,159 @@ pub fn authenticatorClientPin(
                 .keyAgreement = auth.token.getPublicKey(),
             };
         },
+        .changePIN => {
+            if (retry_state.ctr == 0) {
+                return fido.ctap.StatusCodes.ctap2_err_pin_auth_blocked;
+            }
+
+            // 1. If the authenticator does not receive mandatory parameters for this
+            // command, it returns CTAP2_ERR_MISSING_PARAMETER error.
+            if (client_pin_param.pinUvAuthProtocol == null or
+                client_pin_param.keyAgreement == null or
+                client_pin_param.pinHashEnc == null or
+                client_pin_param.newPinEnc == null or
+                client_pin_param.pinUvAuthParam == null)
+            {
+                return fido.ctap.StatusCodes.ctap2_err_missing_parameter;
+            }
+
+            // 2. If pinUvAuthProtocol is not supported, return CTAP1_ERR_INVALID_PARAMETER.
+            if (client_pin_param.pinUvAuthProtocol.? != auth.token.version) {
+                return fido.ctap.StatusCodes.ctap1_err_invalid_parameter;
+            }
+
+            // 3. If the pinRetries counter is 0, return CTAP2_ERR_PIN_BLOCKED error.
+            var settings = auth.callbacks.read_settings();
+
+            if (settings.pinRetries == 0) {
+                return fido.ctap.StatusCodes.ctap2_err_pin_blocked;
+            }
+
+            // 4. The authenticator calls decapsulate on the provided platform
+            // key-agreement key to obtain the shared secret. If an error
+            // results, it returns CTAP1_ERR_INVALID_PARAMETER.
+            const shared_secret = auth.token.ecdh(client_pin_param.keyAgreement.?) catch {
+                return fido.ctap.StatusCodes.ctap1_err_invalid_parameter;
+            };
+
+            // 5. The authenticator calls
+            // verify(shared secret, newPinEnc || pinHashEnc, pinUvAuthParam)
+            const newPinEnc = client_pin_param.newPinEnc.?.get();
+            const pinHashEnc = client_pin_param.pinHashEnc.?.get();
+
+            var buffer: [256]u8 = .{0} ** 256;
+            const len = newPinEnc.len + pinHashEnc.len;
+            @memcpy(buffer[0..newPinEnc.len], newPinEnc);
+            @memcpy(buffer[newPinEnc.len..len], pinHashEnc);
+
+            if (!auth.token.verify(
+                shared_secret.get(),
+                buffer[0..len],
+                client_pin_param.pinUvAuthParam.?.get(),
+            )) {
+                return fido.ctap.StatusCodes.ctap2_err_pin_auth_invalid;
+            }
+
+            // 6. Authenticator decrements the pinRetries counter by 1.
+            settings.pinRetries -= 1;
+            auth.callbacks.write_settings(settings);
+
+            // 7. Authenticator decrypts pinHashEnc
+            // using decrypt(shared secret, pinHashEnc) and verifies against its
+            // internal stored LEFT(SHA-256(curPin), 16).
+            var pinHash: [16]u8 = .{0} ** 16; // LEFT(SHA-256(newPin), 16)
+            auth.token.decrypt(
+                shared_secret.get(),
+                &pinHash,
+                pinHashEnc,
+            );
+
+            const authenticated = auth.callbacks.uv.?(
+                "Change PIN",
+                null,
+                null,
+                &pinHash,
+            );
+            switch (authenticated) {
+                .Denied, .Timeout => {
+                    auth.token.regenerate();
+
+                    if (settings.pinRetries == 0) {
+                        return fido.ctap.StatusCodes.ctap2_err_pin_blocked;
+                    }
+
+                    // TODO check 3 consecutive retires to mitigate
+                    // DOS attacks
+
+                    return fido.ctap.StatusCodes.ctap2_err_pin_invalid;
+                },
+                .AcceptedWithUp => {},
+                .Accepted => {},
+            }
+
+            // 8. Authenticator sets the pinRetries counter to maximum value.
+            settings.uvRetries = 8;
+            auth.callbacks.write_settings(settings);
+
+            // 9. The authenticator calls decrypt(shared secret, newPinEnc) to
+            // produce paddedNewPin. If an error results, it returns
+            // CTAP2_ERR_PIN_AUTH_INVALID.
+
+            // It is important to check that we initialize the
+            // buffer with something other than 0 because the
+            // last byte of the padded pin is 0 and we want to
+            // make sure that it is actually null terminated.
+            var paddedNewPin: [64]u8 = .{'a'} ** 64;
+            auth.token.decrypt(
+                shared_secret.get(),
+                &paddedNewPin,
+                newPinEnc,
+            );
+
+            if (paddedNewPin[63] != 0) {
+                return .ctap2_err_pin_auth_invalid;
+            }
+
+            var i: usize = 63;
+            while (i > 0 and paddedNewPin[i - 1] == 0) i -= 1;
+
+            const newPin = paddedNewPin[0..i];
+
+            if (newPin.len < settings.min_pin_length) {
+                return .ctap2_err_pin_policy_violation;
+            }
+
+            // 13. If the forcePINChange member of the authenticatorGetInfo response
+            // is true and LEFT(SHA-256(newPin), 16) is equal to its internal stored
+            // LEFT(SHA-256(curPin), 16) then authenticator returns
+            // CTAP2_ERR_PIN_POLICY_VIOLATION.
+
+            // TODO: implement force pin change check
+
+            settings.force_pin_change = false;
+            auth.callbacks.write_settings(settings);
+
+            // 17. Authenticator stores LEFT(SHA-256(newPin), 16) internally as the new
+            // value of CurrentStoredPIN.
+
+            var digest: [32]u8 = .{0} ** 32;
+            std.crypto.hash.sha2.Sha256.hash(newPin, &digest, .{});
+
+            auth.callbacks.set_pin.?(digest[0..16]) catch |e| {
+                std.log.err("changePin: unable to set new pin ({any})", .{e});
+                return .ctap1_err_other;
+            };
+
+            // 19. Authenticator calls resetPinUvAuthToken() for all pinUvAuthProtocols
+            // supported by this authenticator. (I.e. all existing pinUvAuthTokens are
+            // invalidated.)
+            auth.token.resetPinUvAuthToken();
+
+            // 20. Authenticator calls resetPersistentPinUvAuthToken() (all
+            // persistent permissions are cleared on pin change).
+
+            // TODO: when implementing persistent pins.
+        },
         .getPinUvAuthTokenUsingUvWithPermissions => {
             if (retry_state.ctr == 0) {
                 return fido.ctap.StatusCodes.ctap2_err_pin_auth_blocked;
@@ -153,6 +306,10 @@ pub fn authenticatorClientPin(
             };
         },
         .getPinUvAuthTokenUsingPinWithPermissions => {
+            if (retry_state.ctr == 0) {
+                return fido.ctap.StatusCodes.ctap2_err_pin_auth_blocked;
+            }
+
             // 1. If the authenticator does not receive mandatory parameters for
             // this command, it returns CTAP2_ERR_MISSING_PARAMETER error.
             if (client_pin_param.pinUvAuthProtocol == null or
